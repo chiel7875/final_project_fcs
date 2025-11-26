d3eb9faf67c69a75457384bd05946f8ef44715b8
사쿠라 <chiel7875@gmail.com>
2025-11-26 23:02:03 +0200

    Add files via upload

--- get_order_by_product.php
+++ get_order_by_product.php
@@ -1,15 +1,14 @@
 <?php
 header("Content-Type: application/json");
 require "config.php";
-$product_id = $_GET["product_id"];
-$sql = "SELECT * FROM orders
-        WHERE product_id = $product_id
-        ORDER BY ordered_at DESC
-        LIMIT 10";
-$result = $conn->query($sql);
-$orders = [];
-while ($row = $result->fetch_assoc()) {
-    $orders[] = $row;
+$raw = file_get_contents("php://input");
+$data = json_decode(file_get_contents("php://input"), true); //hyda new ma kna ekhdinu 
+$product_id = $data["product_id"];
+$quantity   = $data["quantity"];
+$sql = "INSERT INTO orders (product_id, quantity) VALUES ('$product_id', '$quantity')";
+if ($conn->query($sql)) {
+    echo json_encode(["status" => "success"]);
+} else {
+    echo json_encode(["status" => "error", "message" => $conn->error]);
 }
-echo json_encode($orders);
 ?>
